7e4b617eb369c4c219a2176cf4132c11f4a337a6
Taha syeed <85818873+tahasyeed@users.noreply.github.com>
2026-02-05 14:02:17 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="footer">
-      © {new Date().getFullYear()} BBW Queen Victoria. All rights reserved. Made With ❤️ By Taha Syeed
+      © {new Date().getFullYear()} BBW Queen Victoria. All rights reserved. Made With ❤️ By My Boyfriend.
     </footer>
   );
 }
